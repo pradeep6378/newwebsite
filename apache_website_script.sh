2c50eb0f307959cd8bc7461410166a4a111d2401
pradeep6378 <165992016+pradeep6378@users.noreply.github.com>
2024-05-19 21:12:56 +0530

    Delete apache_website_script.sh

--- apache_website_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt update -y
-apt install apache2 -y
-sudo rm -rvf /var/www/html/*
-sudo rm -rvf /var/www/html/.*
-sudo git clone https://github.com/codewithsadee/tourest /var/www/html/
-sudo systemctl restart apache2
-
-
